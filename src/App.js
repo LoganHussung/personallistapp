eb736f3bf87b37c5edd6a17ef7ddb4b97072fc3f
Logan Hussung <hussu072@umn.edu>
2016-12-01 11:26:42 -0600

    Update 1.0

--- src/App.js
+++ src/App.js
@@ -6,26 +6,31 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      toDos: ['Update Portfolio','Update Resume','Apply for Jobs','Get Haircut'],
+      toDos: this.props.toDos,
       newItemValue: ''
     }
   }
   whenSubmit (e){
     e.preventDefault();
     console.log(this.state.newItemValue);
+    var newToDo = this.state.toDos.concat([this.state.newItemValue])
     this.setState({
-      toDos: this.state.toDos.concat([this.state.newItemValue]),
+      toDos: newToDo,
       newItemValue: ''
-    })
+    });
+    localStorage.setItem('toDos', JSON.stringify(newToDo));
   }
+
 
   whenClicked (index, e){
     console.log('people', index);
     var head = this.state.toDos.slice(0,index)
-    var tail = this.state.toDos.slice(index +1, this.state.toDos)
+    var tail = this.state.toDos.slice(index +1, this.state.toDos.length)
+    var newToDo = head.concat(tail)
     this.setState({
-      toDos:head.concat(tail)
-    })
+      toDos: newToDo
+    });
+    localStorage.setItem('toDos', JSON.stringify(newToDo));
   }
   whenChanged (e){
     this.setState({
@@ -33,10 +38,11 @@ class App extends Component {
     })
   }
   render() {
+    console.log(this.props.toDos);
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>ToDo List</h3>
+        <h3>ToDo</h3>
         <form onSubmit={this.whenSubmit.bind(this)}>
           <input type="text" onChange={this.whenChanged.bind(this)} value={this.state.newItemValue} placeholder="New Tasks Here"  />
           <button>Add New Task </button>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
 
+let toDos = JSON.parse(localStorage.getItem('toDos'));
+if (!toDos || toDos.length === 0) {
+  toDos = [
+    'Go to Class',
+    'Learn Stuff',
+    'Get a Job'
+  ];
+  localStorage.setItem('toDos', JSON.stringify(toDos));
+}
 
 ReactDOM.render(
-  <App />,
+  <App toDos={toDos} />,
   document.getElementById('root')
 );
